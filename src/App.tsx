b94b8abd964b05806546f914d8c199c01543d1f3
Sadman600 <sadmansakib600@gmail.com>
2024-01-10 22:53:57 +0600

    added type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import UseStateExample from "./pages/UseStateExample";
 import "./App.css";
+import { useState } from "react";
 function App() {
+  const [counter, setCounter] = useState(0);
+  const handleIncrement = () => {
+    setCounter(counter + 1);
+  };
+  // const handleDecrement = () => {
+  //   setCounter(counter + 1);
+  // };
+  // const handleReset = () => {
+  //   setCounter(0);
+  // };
   return (
     <>
       <div className="appContainer">
-        <UseStateExample />
+        <UseStateExample
+          counter={counter}
+          setCounter={setCounter}
+          handleIncrement={handleIncrement}
+        />
       </div>
     </>
   );
--- src/pages/UseStateExample.tsx
+++ src/pages/UseStateExample.tsx
@@ -1,28 +1,25 @@
 import { Button } from "@mui/material";
-import { useState } from "react";
 
-const UseStateExample = () => {
-  const [counter, setCounter] = useState(0);
+type TCounter = {
+  counter: number;
+  setCounter: React.Dispatch<React.SetStateAction<number>>;
+  handleIncrement: () => void;
+};
+const UseStateExample = (props: TCounter) => {
+  const { counter, handleIncrement } = props;
+
   return (
     <div>
       <h1>{counter}</h1>
-      <Button
-        variant="outlined"
-        color="success"
-        onClick={() => setCounter(counter + 1)}
-      >
+      <Button variant="outlined" color="success" onClick={handleIncrement}>
         Increment
       </Button>
-      <Button
-        variant="outlined"
-        color="success"
-        onClick={() => setCounter(counter - 1)}
-      >
+      {/* <Button variant="outlined" color="success" onClick={handleDecrement}>
         Decrement
       </Button>
-      <Button variant="outlined" color="error" onClick={() => setCounter(0)}>
+      <Button variant="outlined" color="error" onClick={handleReset}>
         Reset
-      </Button>
+      </Button> */}
     </div>
   );
 };
