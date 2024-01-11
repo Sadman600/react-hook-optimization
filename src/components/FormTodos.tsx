cfc59b6ad0cdd40db0bdd148912616bd97e56114
Sadman600 <sadmansakib600@gmail.com>
2024-01-11 23:32:19 +0600

    form todos

--- src/components/FormTodos.tsx
+++ src/components/FormTodos.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 const FormTodos = () => {
-  return <div></div>;
+  const initialstate = { name: "", email: "" };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "name":
+        return { ...state, name: action.payload };
+      case "email":
+        return { ...state, email: action.payload };
+    }
+  };
+  const [users, dispatch] = useReducer(reducer, initialstate);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(users);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={(e) =>
+            dispatch({ type: e.target.name, payload: e.target.value })
+          }
+        />
+        <br />
+        <input
+          type="text"
+          name="email"
+          id="name"
+          onChange={(e) =>
+            dispatch({ type: e.target.name, payload: e.target.value })
+          }
+        />
+        <br />
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default FormTodos;
